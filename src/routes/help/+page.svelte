6b6436ed636d76432a25fae2b4cb7a6045e1e956
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-02-22 10:42:44 +0100

    added help page

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,43 @@
+<div>
+  <h1>Hjelp</h1>
+  <br />
+  <h2>Begrepsforklaringer</h2>
+  <h3>UPN (UserPrincipalName)</h3>
+  <p>
+    Adressen du logger deg på Office 365 med. Dette skal også være likt som e-postadressen<br />
+    Eksempel: <code>bjarne.betjent@vtfk.no</code>
+  </p>
+  <br />
+  <h3>SamAccountName</h3>
+  <p>
+    Brukernavnet ditt (kortversjonen).<br />
+    Eksempel: <code>bja0101</code>
+  </p>
+  <br />
+  <h3>UID</h3>
+  <p>
+    FEIDE-brukernavn<br />
+    Eksempel: <code>bja0101</code>
+  </p>
+  <br />
+  <h3>PrincipalName</h3>
+  <p>
+    Fullt FEIDE-navn<br />
+    Eksempel: <code>bja0101@vtfk.no</code>
+  </p>
+  <br />
+  <h3>MFA</h3>
+  <p>
+    Multi Factor Authentication (2faktor i Azure)
+  </p>
+  <br />
+  <h3>OU (OrganizationalUnit)</h3>
+  <p>
+    Mappestruktur i Active Directory
+  </p>
+  <br />
+</div>
+
+
+<style>
+</style>
